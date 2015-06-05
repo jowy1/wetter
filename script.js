0042d4b224439c96157b006afd84281eabd347ed
Joel Wyss <joel@Joel-Wysss-MacBook-Pro.local>
2015-06-05 11:21:08 +0200

    index.html

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ $(document).ready(function(){
 		console.log(position);
 		$('.longitude').text(position.coords.longitude);
 		$('.latitude').text(position.coords.latitude);
-		$('accuracy').text(position.coords.accuracy);
+		$('.accuracy').text(position.coords.accuracy);
 	});
 });
