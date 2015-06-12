a42c29b5a1becf6fc3c43cdb3645b42155ae09f2
Joel Wyss <joel@Joel-Wyss-MacBook-Pro.local>
2015-06-12 10:59:35 +0200

    index.html

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ $(document).ready(function(){
 
 			console.log(data);
 
-			$('.temperature').text(data.currently.apparentTemperature) + "°C";
+			$('.temperature').text(data.currently.apparentTemperature + "°C");
 
 
 			//geocode anfrage
