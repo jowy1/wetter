57908d710a2d1b488a54552d8a63157a7b19de94
Joel Wyss <joel@Joel-Wyss-MacBook-Pro.local>
2015-06-12 09:06:48 +0200

    index.html

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
 $(document).ready(function(){
 	navigator.geolocation.getCurrentPosition(function(position){
 
-		console.log(position);
-		$('.longitude').text(position.coords.longitude);
-		$('.latitude').text(position.coords.latitude);
-		$('.accuracy').text(position.coords.accuracy);
+		var koordinaten = {
+
+			longitude: position.coords.longitude,
+			latitude: position.coords.latitude
+		};
+
+		$.ajax({
+
+			url: 'https://api.forecast.io/forecast/b7cadfacd8c9a274d6aec6b01618e241/' + koordinaten.latitude + ',' + koordinaten.longitude,
+
+			data:
+
+				units: 'si',
+				lang: 'de'
+
+			}
+
+			dataType: 'jsonp'
+
+		}).done(function()
+
+			console.log(data);
+
+
+		});
+
+		//console.log(position);
+		//$('.longitude').text(position.coords.longitude);
+		//$('.latitude').text(position.coords.latitude);
+		//$('.accuracy').text(position.coords.accuracy);
 	});
 });
